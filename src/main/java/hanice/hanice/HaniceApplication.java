9f1b54d9b075b0e5177c9aa5a67c5192add477ea
bingHan <bingHan>
2022-06-21 09:25:38 +0800

    yml

---
+++ src/main/java/hanice/hanice/HaniceApplication.java
@@ -0,0 +1,16 @@
+package hanice.hanice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Hanice
+ */
+@SpringBootApplication
+public class HaniceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaniceApplication.class, args);
+    }
+
+}
